b9e5916c53f92665074ada53e5f1488cb3cba773
Bachir Seghir <bachir.seghir17@gmail.com>
2021-03-19 15:55:10 +0100

    user created in Keystone

---
+++ backend/keystone.ts
@@ -0,0 +1,56 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO : add in initial roles
+  },
+});
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: add data seeding
+    },
+    lists: createSchema({
+      // schemas item go in here
+      User,
+    }),
+    ui: {
+      // TODO : change this for roles
+      isAccessAllowed: ({ session }) => {
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: `id email name`,
+    }),
+  })
+);
